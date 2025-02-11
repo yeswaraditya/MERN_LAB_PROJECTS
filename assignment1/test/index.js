91f368c1f1b5ff0f2d41714d104f224d202eecd2
Eswar <yeswaraditya3006@gmail.com>
2025-02-11 16:20:18 +0530

    assignment1

---
+++ assignment1/test/index.js
@@ -0,0 +1,4 @@
+import { sumOfNumbers } from "./node_modules/eswar_pkg_mern/index";
+
+
+console.log(sumOfNumbers(5)); 
---
+++ assignment1/test/node_modules/eswar_pkg_mern/index.js
@@ -0,0 +1,8 @@
+export function sumOfNumbers(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
